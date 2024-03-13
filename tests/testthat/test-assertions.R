2ddf58a291284d0dc9db4e6861fedf68dcee9110
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-03-13 15:05:35 -0700

    Update test-assertions.R

--- tests/testthat/test-assertions.R
+++ tests/testthat/test-assertions.R
@@ -47,7 +47,7 @@ test_that("assert_filter_cond Test 3: `assert_filter_cond` works as expected", {
   fc <- expr("string")
   expect_error(
     assert_filter_cond(arg = fc),
-    class  = "assert_filter_cond"
+    class = "assert_filter_cond"
   )
   expect_snapshot(
     assert_filter_cond(arg = fc),
