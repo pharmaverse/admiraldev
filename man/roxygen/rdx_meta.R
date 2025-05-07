8724d99168ca7f04bd6ebfdd101edf29e48d2f49
Stefan Bundfuss <80953585+bundfussr@users.noreply.github.com>
2025-05-07 22:28:01 +0200

    Closes #490 fix_replace_symbol_in_expr: handle NAs and recursions (#491)
    #490 fix_replace_symbol_in_expr: handle NAs and recursions in replace_symbol_in_expr()

--- man/roxygen/rdx_meta.R
+++ man/roxygen/rdx_meta.R
@@ -1,6 +1,7 @@
 list(
   rdx_permitted_values = list(
     char_scalar = "a character scalar, i.e., a character vector of length one",
+    symbol = "an unquoted symbol, e.g., `AVAL`",
     var_list = "list of variables created by `exprs()`, e.g., `exprs(USUBJID, VISIT)`"
   )
 )
