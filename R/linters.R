13e572653e8991cd4867ebba314182028d6f1add
Edoardo Mancini <edoardo.mancini@roche.com>
2025-09-04 15:01:20 +0000

    #506 add arguments to control linters

--- R/linters.R
+++ R/linters.R
@@ -3,8 +3,15 @@
 #' This function lists out the linter configurations for the `{admiral}`
 #' family of packages.
 #'
+#' @param line_length Line length linter
+#' @param object_usage Object usage linter
+#' @param cyclocomp Cyclomatic complexity linter
+#' @param indentation Indentation linter
+#' @param undesirable_function Undesirable function linter
+#' @param ... Any additional default linters to specify for use within the package
+#'
 #' @details
-#' Configurations of special note:
+#' Default configurations of special note:
 #'   - Enforcing the use of `cli::cli_abort()` over `stop()`
 #'   - Enforcing the use of `cli::cli_warn()` over `warning()`
 #'   - Enforcing the use of `cli::cli_inform()` over `message()`
@@ -12,24 +19,39 @@
 #'   - Enforcing the use of `stringr::str_detect()` over `grepl()`
 #'
 #' @keywords internal
-admiral_linters <- function() {
+#'
+#' @examples
+#' # The code below should reside in .lintr.R file
+#' linters <- admiral_linters(
+#'   line_length = line_length_linter(80),
+#'   indentation = NULL
+#' )
+#'
+admiral_linters <- function(line_length = line_length_linter(100),
+                            object_usage = NULL,
+                            cyclocomp = cyclocomp_linter(complexity_limit = 20),
+                            indentation = NULL,
+                            undesirable_function = undesirable_function_linter(
+                              fun = c(
+                                default_undesirable_functions,
+                                "stop" = "please use cli_abort() from the {cli} package",
+                                "warning" = "please use cli_warning() from the {cli} package",
+                                "message" = "please use cli_inform() from the {cli} package",
+                                "ifelse" = "please use vectorized if_else() from the {dplyr} package",
+                                "grepl" = "please use str_detect() from the {stringr} package"
+                              )
+                            ),
+                            ...) {
   c(
-    # Default linters
     linters_with_defaults(
-      line_length_linter(100),
-      object_usage_linter = NULL,
-      cyclocomp_linter(complexity_limit = 20),
-      indentation_linter = NULL,
-      undesirable_function_linter = undesirable_function_linter(
-        fun = c(
-          default_undesirable_functions,
-          "stop" = "please use cli_abort() from the {cli} package",
-          "warning" = "please use cli_warning() from the {cli} package",
-          "message" = "please use cli_inform() from the {cli} package",
-          "ifelse" = "please use vectorized if_else() from the {dplyr} package",
-          "grepl" = "please use str_detect() from the {stringr} package"
-        )
-      )
+      # Add default linters below
+      line_length_linter = line_length,
+      object_usage_linter = object_usage,
+      cyclocomp_linter = cyclocomp,
+      indentation_linter = indentation,
+      undesirable_function_linter = undesirable_function,
+      ...
     )
+    # Add custom linters below
   )
 }
