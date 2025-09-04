7d6df2e2dcb3d44dcb5d1a7edc73076617a7eb5b
Edoardo Mancini <edoardo.mancini@roche.com>
2025-09-04 14:18:57 +0000

    #506 add more forbidden functions

--- R/linters.R
+++ R/linters.R
@@ -7,6 +7,8 @@
 #' Configurations of special note:
 #'   - Enforcing the use of `cli::cli_abort()` over `stop()`
 #'   - Enforcing the use of `cli::cli_warn()` over `warning()`
+#'   - Enforcing the use of  vectorized `dplyr::if_else()`  over `ifelse()`
+#'   - Enforcing the use of `stringr::str_detect()` over `grepl()`
 #'
 #' @keywords internal
 admiral_linters <- function() {
@@ -16,14 +18,16 @@ admiral_linters <- function() {
       line_length_linter(100),
       object_usage_linter = NULL,
       cyclocomp_linter(complexity_limit = 20),
+      indentation_linter = NULL,
       undesirable_function_linter = undesirable_function_linter(
         fun = c(
           default_undesirable_functions,
           "stop" = "please use cli_abort() from the {cli} package",
-          "warning" = "please use cli_warning() from the {cli} package"
+          "warning" = "please use cli_warning() from the {cli} package",
+          "ifelse" = "please use vectorized if_else() from the {dplyr} package",
+          "grepl" = "please use str_detect() from the {stringr} package"
         )
-      ),
-      indentation_linter = NULL
+      )
     )
   )
 }
