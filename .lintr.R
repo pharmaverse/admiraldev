b6cac89c1db3eb2353961986ca975ab11278d6ac
Stefan Bundfuss <stefan.bundfuss@roche.com>
2025-11-05 15:18:55 +0000

    #512 allow_r6_docu: fix lintr and test man check

--- .lintr.R
+++ .lintr.R
@@ -2,7 +2,7 @@ library(lintr)
 
 source("inst/lintr/linters.R")
 
-linters <- admiral_linters()
+linters <- admiral_linters(cyclocomp = cyclocomp_linter(complexity_limit = 21))
 
 exclusions <- list(
   "R/data.R" = Inf,
