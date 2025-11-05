77e73b9d8bd6bb9d9a50aec678b496db890d8704
Stefan Bundfuss <80953585+bundfussr@users.noreply.github.com>
2025-11-05 17:20:19 +0100

    Closes #512 allow_r6_docu: update roclet to enable R6 documentation (#513)
    * #512 allow_r6_docu: update roclet to enable R6 documentation
    
    * #512 allow_r6_docu: update man
    
    * #512 allow_r6_docu: fix lintr and test man check
    
    * #512 allow_r6_docu: remove old R CMD check
    
    * #512 allow_r6_docu: stlye files and use main man check

---
+++ tests/testthat/fixtures/test_class.R
@@ -0,0 +1,15 @@
+#' A Test R6 Class
+#'
+TestClass <- R6::R6Class( # nolint: object_name_linter
+  "TestClass",
+  public = list(
+    #' @field field A field
+    field = NULL,
+
+    #' @description
+    #' Initialize a new `TestClass` object
+    initialize = function(field) {
+      self$field <- field
+    }
+  )
+)
