e3f16a6331d8fe91337f36de546aef870e21ce64
Edoardo Mancini <edoardo.mancini@roche.com>
2026-09-21 14:00:21 +0000

    Updates following review

--- R/admiraldev-package.R
+++ R/admiraldev-package.R
@@ -1,44 +1,31 @@
 #' @keywords internal
-#' @importFrom dplyr across arrange bind_rows case_when
-#'   desc ends_with filter full_join group_by
-#'   if_else mutate n pull rename row_number
-#'   select slice starts_with transmute ungroup
-#'   n_distinct union distinct summarise coalesce bind_cols
-#'   na_if tibble %>%
+#' @importFrom dplyr across arrange desc filter group_by
+#'   if_else mutate n pull rename select starts_with
+#'   ungroup n_distinct summarise tibble %>%
 #'
-#' @importFrom rlang := abort arg_match as_label as_name as_string
-#'   call2 caller_env call_name current_env .data
-#'   enexpr env eval_bare eval_tidy expr expr_deparse
-#'   expr_interp expr_label exprs f_lhs f_rhs global_env
-#'   is_named missing_arg is_bare_formula is_call
-#'   is_character is_expression is_formula is_integerish
-#'   is_logical is_missing is_symbol new_environment
-#'   parse_expr parse_exprs set_names sym syms
-#'   type_of warn %||%
+#' @importFrom rlang := abort as_label as_string caller_env
+#'   current_env .data enexpr env expr expr_deparse
+#'   exprs f_lhs f_rhs global_env is_named is_call
+#'   is_expression is_formula is_integerish is_logical
+#'   is_missing is_symbol new_environment parse_expr
+#'   set_names sym syms warn %||%
 #'
-#' @importFrom utils capture.output str
+#' @importFrom utils capture.output
 #'
-#' @importFrom purrr discard map map2 map_chr map_lgl reduce walk
-#'   keep map_if transpose list_flatten every
-#'   modify_at modify_if reduce compose
+#' @importFrom purrr discard map map2 map_chr map_lgl
+#'   keep list_flatten every
 #'
 #' @importFrom roxygen2 rd_section roclet roclet_process roxy_tag
 #'   roxy_tag_parse roxy_tag_rd tag_markdown
 #'   warn_roxy_tag
 #'
-#' @importFrom stringr str_c str_detect str_extract str_glue str_match
-#'   str_remove str_remove_all str_replace
-#'   str_replace_all str_trim str_to_lower str_subset
-#'   str_to_title str_to_upper
+#' @importFrom stringr str_c str_detect str_extract str_match
+#'   str_remove str_remove_all str_replace_all str_subset
 #'
-#' @importFrom lubridate as_datetime ceiling_date date days duration
-#'   floor_date is.Date is.instant time_length %--%
-#'   ymd ymd_hms weeks years hours minutes
-#'   parse_date_time
+#' @importFrom lubridate date days is.instant ymd years hours
+#'   minutes parse_date_time
 #'
-#' @importFrom tidyr drop_na nest pivot_longer pivot_wider unnest
-#'
-#' @importFrom tidyselect all_of contains vars_select
+#' @importFrom tidyselect contains vars_select
 #'
 #' @importFrom lifecycle deprecate_warn deprecated deprecate_stop
 #'   deprecate_soft
